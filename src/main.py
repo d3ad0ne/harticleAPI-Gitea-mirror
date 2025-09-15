06e24e35e17d1774651c902b0210fc0c87d1417b
n0one <n0one@noreply.localhost>
2025-09-15 14:36:54 +0300

    Changed 127.0.0.1 to 0.0.0.0

--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ from app_creator import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    uvicorn.run(app=app, host="127.0.0.1", port=8000, log_level=uvicorn_logging_level.lower())
+    uvicorn.run(app=app, host="0.0.0.0", port=8000, log_level=uvicorn_logging_level.lower())
